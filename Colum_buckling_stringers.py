d1fd2a7202334c1d23d5ebc43282d4e09a25b9d5
gabolas999 <gabo.g.cool@gmail.com>
2022-12-07 16:14:57 +0100

    c4.2

--- Colum_buckling_stringers.py
+++ Colum_buckling_stringers.py
@@ -9,3 +9,9 @@ from math import *
 #centroid z direction
 Str_z_tilda_= (Var.Str_v_len*Var.Str_v_th*Var.Str_v_len/2)/Var.Str_A
 Str_MOI_X = (Var.Str_h_len*Var.Str_h_th**3/12 + Var.Str_h_len*Var.Str_h_th*(Str_z_tilda_ - Var.Str_h_th/2)**2) + (Var.Str_v_len**3*Var.Str_v_th/12 + Var.Str_v_len*Var.Str_v_th * (Var.Str_v_len/2-Str_z_tilda_)**2) 
+
+def stress_b_Str():
+    sigma = (Var.k * pi)/(1)
+    return 
+
+print(pi)
