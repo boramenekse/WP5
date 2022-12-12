d35f2a678ba7df0023a2a770e498d4dbd6b00093
boramenekse <boramenekse02@gmail.com>
2022-12-12 15:50:09 +0100

    C5.1

--- Normal_stress_distribution.py
+++ Normal_stress_distribution.py
@@ -8,7 +8,26 @@ import Copy_Variables_for_crossection as var
 #loading case 16 and loading case 12
 #jump in the moment diagram 11.69m
 
+a = smp.symbols('a', real=True)
+h = smp.symbols('h', real=True)
+d = smp.symbols('d', real=True, positive=True)
+t = smp.symbols('t', real=True)
 x = smp.symbols('x', real=True)
+y = smp.symbols('y', real=True)
+r = smp.symbols('r', real=True, positive=True)
+alpha = smp.symbols('\u03B1', real=True)
+beta = smp.symbols('\u03B2', real=True)
+rho = smp.symbols('\u03C1', real=True)
+sigma = smp.symbols('\u03C3', real=True)
+theta = smp.symbols('\u03B8', real=True)
+eta = smp.symbols('\u03B7', real=True)
+xi = smp.symbols('\u03BE', real=True)
+ixx = smp.symbols('Ixx', real=True)
+iyy = smp.symbols('Ixy', real=True)
+ixy = smp.symbols('Iyy', real=True)
+mx = smp.symbols('Mx', real=True)
+my = smp.symbols('My', real=True)
+
 M1_12 = [-0.19569866609295541, 6.6801804985865445, 347.650403262873, -3851.0392032213094, -529912.4293065271, 13329458.866177564] 
 M2_12 = [-0.19569866608662626, 6.680180498386035, 347.65040326524246, -3851.039203234463, -684065.9293064886, 14011505.048648307]
 M1_16 = [0.40016072099856226, -9.397942238957981, -1047.384217723905, 6708.870613191617, 1582191.8673072485, -33488163.089606084] 
@@ -28,12 +47,16 @@ heaviside = smp.Heaviside(x-11.69, 0)
 mx_fun_12 = md_fun1_12-md_fun1_12*heaviside+md_fun2_12*heaviside
 mx_fun_16 = md_fun1_16-md_fun1_16*heaviside+md_fun2_16*heaviside
 Ny_fun = -500000 + 500000*heaviside
+mz_fun = -6008660+514000*x-(-6008660+514000*x)*heaviside
 
 span = np.linspace(0, 0.5*var.Span, 1000, endpoint=True)
 plt.figure()
 plt.plot(span, -smp.lambdify([x], mx_fun_12)(span[0:]))
 plt.plot(span, -smp.lambdify([x], mx_fun_16)(span[0:]))
 plt.plot(span, smp.lambdify([x], Ny_fun)(span[0:]))
+plt.plot(span, smp.lambdify([x], mz_fun)(span[0:]))
 plt.show()
 
-
+def sigma_z(mx, my, ixx, iyy, ixy, x, y):
+  exp = ((mx*iyy-my*ixy)*y+(my*ixx-mx*ixy)*x)/(ixx*iyy-ixy**2)
+  return exp
