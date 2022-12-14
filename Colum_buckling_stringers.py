e2ef6a6b9962b544daa897e3a9ac7f03272ec876
gabolas999 <gabo.g.cool@gmail.com>
2022-12-14 14:45:41 +0100

    c5.2

--- Colum_buckling_stringers.py
+++ Colum_buckling_stringers.py
@@ -10,8 +10,8 @@ from math import *
 Str_z_tilda_= (Var.Str_v_len*Var.Str_v_th*Var.Str_v_len/2)/Var.Str_A
 Str_MOI_X = (Var.Str_h_len*Var.Str_h_th**3/12 + Var.Str_h_len*Var.Str_h_th*(Str_z_tilda_ - Var.Str_h_th/2)**2) + (Var.Str_v_len**3*Var.Str_v_th/12 + Var.Str_v_len*Var.Str_v_th * (Var.Str_v_len/2-Str_z_tilda_)**2) 
 
-def critical_stress_b_Str(y):
-    sigma = (Var.k * pi**2 *Var.e_mod *Str_MOI_X)/(y**2*Var.Str_A)
+def critical_stress_b_Str(L):
+    sigma = (Var.k * pi**2 *Var.e_mod *Str_MOI_X)/(L**2*Var.Str_A)
     return sigma
 
 print(critical_stress_b_Str(1))
