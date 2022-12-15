82657b8021729dc58741a2bee18833ac9077a068
Sailor1026 <tijs.delaere2003@gmail.com>
2022-12-15 16:32:06 +0100

    C5.3

--- Shear_buckling_spar_web.py
+++ Shear_buckling_spar_web.py
@@ -94,6 +94,7 @@ plt.plot(y_list, critical_shear_stress_rear_spar_list_3)
 plt.legend(["forward_1", "rear_1", "forward_2", "rear_2", "forward_3", "rear_3"])
 plt.xlabel('Span [m]')
 plt.ylabel('Critical Buckling Shear Stress [Pa]')
+plt.grid()
 plt.show()
 
 ##### GET THE SHEAR STRESS DUE TO SHEAR FORCE
@@ -302,33 +303,34 @@ for i in range(len(y_list)):
 
 #Desing philosophy 1
 #Front
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-fig.subplots_adjust(hspace=0.05)
-ax1.plot(y_list, critical_shear_stress_forward_spar_list_1, '-', label='Tau_Crit')
-ax1.plot(y_list, shear_stress_LC_8_1_forward, '-', label='Tau_LC8')
-ax1.plot(y_list, shear_stress_LC_12_1_forward, '-', label='Tau_LC12')
-ax1.plot(y_list, shear_stress_LC_16_1_forward, '-', label='Tau_LC16')
-plt.suptitle('Shear stress in the forward spar, philosophy 1')
-ax2.plot(y_list, critical_shear_stress_forward_spar_list_1, '-', label='Tau_Crit')
-ax2.plot(y_list, shear_stress_LC_8_1_forward, '-', label='Tau_LC8')
-ax2.plot(y_list, shear_stress_LC_12_1_forward, '-', label='Tau_LC12')
-ax2.plot(y_list, shear_stress_LC_16_1_forward, '-', label='Tau_LC16')
-ax1.legend(loc = 'upper right')
+#fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+#fig.subplots_adjust(hspace=0.05)
+plt.plot(y_list, critical_shear_stress_forward_spar_list_1, '-', label='Tau_Crit')
+plt.plot(y_list, shear_stress_LC_8_1_forward, '-', label='Tau_LC8')
+plt.plot(y_list, shear_stress_LC_12_1_forward, '-', label='Tau_LC12')
+plt.plot(y_list, shear_stress_LC_16_1_forward, '-', label='Tau_LC16')
+plt.title('Shear stress in the forward spar, philosophy 1')
+#ax2.plot(y_list, critical_shear_stress_forward_spar_list_1, '-', label='Tau_Crit')
+#ax2.plot(y_list, shear_stress_LC_8_1_forward, '-', label='Tau_LC8')
+#ax2.plot(y_list, shear_stress_LC_12_1_forward, '-', label='Tau_LC12')
+#ax2.plot(y_list, shear_stress_LC_16_1_forward, '-', label='Tau_LC16')
+plt.legend(loc = 'upper right')
 plt.xlabel('Span [m]')
-fig.supylabel('Shear stress [Pa]')
+plt.ylabel('Shear stress [Pa]')
 minimum = min(min(shear_stress_LC_8_1_forward), min(shear_stress_LC_12_1_forward), min(shear_stress_LC_16_1_forward))
 maximum = max(max(shear_stress_LC_8_1_forward), max(shear_stress_LC_12_1_forward), max(shear_stress_LC_16_1_forward))
-ax1.set_ylim(critical_shear_stress_forward_spar_list_1[0] - 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))), critical_shear_stress_forward_spar_list_1[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
-ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
-ax1.spines.bottom.set_visible(False)
-ax2.spines.top.set_visible(False)
-ax1.xaxis.tick_top()
-ax1.tick_params(labeltop=False)  # don't put tick labels at the top
-ax2.xaxis.tick_bottom()
-d = .5  # proportion of vertical to horizontal extent of the slanted line
-kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
-ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
-ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), critical_shear_stress_forward_spar_list_1[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
+#ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
+#ax1.spines.bottom.set_visible(False)
+#ax2.spines.top.set_visible(False)
+#ax1.xaxis.tick_top()
+#ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+#ax2.xaxis.tick_bottom()
+#d = .5  # proportion of vertical to horizontal extent of the slanted line
+#kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+#ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+#ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
@@ -367,6 +369,8 @@ d = .5  # proportion of vertical to horizontal extent of the slanted line
 kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
 ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
 ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+ax1.grid()
+ax2.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
@@ -406,6 +410,8 @@ d = .5  # proportion of vertical to horizontal extent of the slanted line
 kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
 ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
 ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+ax1.grid()
+ax2.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
@@ -444,6 +450,8 @@ d = .5  # proportion of vertical to horizontal extent of the slanted line
 kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
 ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
 ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+ax1.grid()
+ax2.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
@@ -456,33 +464,34 @@ if l == len(y_list):
 
 #Desing philosophy 3
 #Front
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-fig.subplots_adjust(hspace=0.05)
-ax1.plot(y_list, critical_shear_stress_forward_spar_list_3, '-', label='Tau_Crit')
-ax1.plot(y_list, shear_stress_LC_8_3_forward, '-', label='Tau_LC8')
-ax1.plot(y_list, shear_stress_LC_12_3_forward, '-', label='Tau_LC12')
-ax1.plot(y_list, shear_stress_LC_16_3_forward, '-', label='Tau_LC16')
-plt.suptitle('Shear stress in the forward spar, philosophy 3')
-ax2.plot(y_list, critical_shear_stress_forward_spar_list_3, '-', label='Tau_Crit')
-ax2.plot(y_list, shear_stress_LC_8_3_forward, '-', label='Tau_LC8')
-ax2.plot(y_list, shear_stress_LC_12_3_forward, '-', label='Tau_LC12')
-ax2.plot(y_list, shear_stress_LC_16_3_forward, '-', label='Tau_LC16')
-ax1.legend(loc = 'upper right')
+#fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+#fig.subplots_adjust(hspace=0.05)
+plt.plot(y_list, critical_shear_stress_forward_spar_list_3, '-', label='Tau_Crit')
+plt.plot(y_list, shear_stress_LC_8_3_forward, '-', label='Tau_LC8')
+plt.plot(y_list, shear_stress_LC_12_3_forward, '-', label='Tau_LC12')
+plt.plot(y_list, shear_stress_LC_16_3_forward, '-', label='Tau_LC16')
+plt.title('Shear stress in the forward spar, philosophy 3')
+#ax2.plot(y_list, critical_shear_stress_forward_spar_list_3, '-', label='Tau_Crit')
+#ax2.plot(y_list, shear_stress_LC_8_3_forward, '-', label='Tau_LC8')
+#ax2.plot(y_list, shear_stress_LC_12_3_forward, '-', label='Tau_LC12')
+#ax2.plot(y_list, shear_stress_LC_16_3_forward, '-', label='Tau_LC16')
+plt.legend(loc = 'upper right')
 plt.xlabel('Span [m]')
-fig.supylabel('Shear stress [Pa]')
+plt.ylabel('Shear stress [Pa]')
 minimum = min(min(shear_stress_LC_8_3_forward), min(shear_stress_LC_12_3_forward), min(shear_stress_LC_16_3_forward))
 maximum = max(max(shear_stress_LC_8_3_forward), max(shear_stress_LC_12_3_forward), max(shear_stress_LC_16_3_forward))
-ax1.set_ylim(critical_shear_stress_forward_spar_list_3[0] - 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))), critical_shear_stress_forward_spar_list_3[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
-ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
-ax1.spines.bottom.set_visible(False)
-ax2.spines.top.set_visible(False)
-ax1.xaxis.tick_top()
-ax1.tick_params(labeltop=False)  # don't put tick labels at the top
-ax2.xaxis.tick_bottom()
-d = .5  # proportion of vertical to horizontal extent of the slanted line
-kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
-ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
-ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), critical_shear_stress_forward_spar_list_3[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
+#ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
+#ax1.spines.bottom.set_visible(False)
+#ax2.spines.top.set_visible(False)
+#ax1.xaxis.tick_top()
+#ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+#ax2.xaxis.tick_bottom()
+#d = .5  # proportion of vertical to horizontal extent of the slanted line
+#kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+#ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+#ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
@@ -494,33 +503,34 @@ if l == len(y_list):
     print("philosophy 3, forward spar: pass")
 
 #Rear
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-fig.subplots_adjust(hspace=0.05)
-ax1.plot(y_list, critical_shear_stress_rear_spar_list_3, '-', label='Tau_Crit')
-ax1.plot(y_list, shear_stress_LC_8_3_rear, '-', label='Tau_LC8')
-ax1.plot(y_list, shear_stress_LC_12_3_rear, '-', label='Tau_LC12')
-ax1.plot(y_list, shear_stress_LC_16_3_rear, '-', label='Tau_LC16')
-plt.suptitle('Shear stress in the rear spar, philosophy 3')
-ax2.plot(y_list, critical_shear_stress_rear_spar_list_3, '-', label='Tau_Crit')
-ax2.plot(y_list, shear_stress_LC_8_3_rear, '-', label='Tau_LC8')
-ax2.plot(y_list, shear_stress_LC_12_3_rear, '-', label='Tau_LC12')
-ax2.plot(y_list, shear_stress_LC_16_3_rear, '-', label='Tau_LC16')
-ax1.legend(loc = 'upper right')
+#fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+#fig.subplots_adjust(hspace=0.05)
+plt.plot(y_list, critical_shear_stress_rear_spar_list_3, '-', label='Tau_Crit')
+plt.plot(y_list, shear_stress_LC_8_3_rear, '-', label='Tau_LC8')
+plt.plot(y_list, shear_stress_LC_12_3_rear, '-', label='Tau_LC12')
+plt.plot(y_list, shear_stress_LC_16_3_rear, '-', label='Tau_LC16')
+plt.title('Shear stress in the rear spar, philosophy 3')
+#ax2.plot(y_list, critical_shear_stress_rear_spar_list_3, '-', label='Tau_Crit')
+#ax2.plot(y_list, shear_stress_LC_8_3_rear, '-', label='Tau_LC8')
+#ax2.plot(y_list, shear_stress_LC_12_3_rear, '-', label='Tau_LC12')
+#ax2.plot(y_list, shear_stress_LC_16_3_rear, '-', label='Tau_LC16')
+plt.legend(loc = 'upper right')
 plt.xlabel('Span [m]')
-fig.supylabel('Shear stress [Pa]')
+plt.ylabel('Shear stress [Pa]')
 minimum = min(min(shear_stress_LC_8_3_rear), min(shear_stress_LC_12_3_rear), min(shear_stress_LC_16_3_rear))
 maximum = max(max(shear_stress_LC_8_3_rear), max(shear_stress_LC_12_3_rear), max(shear_stress_LC_16_3_rear))
-ax1.set_ylim(critical_shear_stress_rear_spar_list_3[0] - 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))), critical_shear_stress_rear_spar_list_3[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
-ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
-ax1.spines.bottom.set_visible(False)
-ax2.spines.top.set_visible(False)
-ax1.xaxis.tick_top()
-ax1.tick_params(labeltop=False)  # don't put tick labels at the top
-ax2.xaxis.tick_bottom()
-d = .5  # proportion of vertical to horizontal extent of the slanted line
-kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
-ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
-ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), critical_shear_stress_rear_spar_list_3[0] + 0.5 * (maximum - minimum + max(abs(minimum), abs(maximum))))  # outliers only
+#ax2.set_ylim(minimum - 0.5 * max(abs(minimum), abs(maximum)), maximum + 0.5 * max(abs(minimum), abs(maximum)))
+#ax1.spines.bottom.set_visible(False)
+#ax2.spines.top.set_visible(False)
+#ax1.xaxis.tick_top()
+#ax1.tick_params(labeltop=False)  # don't put tick labels at the top
+#ax2.xaxis.tick_bottom()
+#d = .5  # proportion of vertical to horizontal extent of the slanted line
+#kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+#ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+#ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+plt.grid()
 plt.show()
 l=0
 for i in range(len(y_list)):
